c6683be0f5c975849cedf51b4f1980d89142a7b7
Pasindu Kavishka <IT19123332@my.sliit.lk>
2021-08-21 03:32:46 +0530

    commit 2

--- app/src/main/java/com/example/additionalsubmission/MainActivity.java
+++ app/src/main/java/com/example/additionalsubmission/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.additionalsubmission;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+
     }
+
+
 }
